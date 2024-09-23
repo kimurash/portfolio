081c2acc5dbd08fd0671bbee41396a2f0ccd958e
shunsei <kimshun1227@icloud.com>
2024-09-24 03:22:54 +0900

    Show result message

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -2,15 +2,33 @@ window.onload = function() {
     document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();
 
+        let thisForm = this;
+
+        thisForm.querySelector('.loading').classList.add('d-block');
+        thisForm.querySelector('.error-message').classList.remove('d-block');
+        thisForm.querySelector('.sent-message').classList.remove('d-block');
+
         // generate a five digit number for the contact_number variable
-        this.contact_number.value = Math.random() * 100000 | 0;
+        thisForm.contact_number.value = Math.random() * 100000 | 0;
 
         // these IDs from the previous steps
         emailjs.sendForm('service_mkob74b', 'template_mj7yns8', this)
+            .finally(() => {
+                thisForm.querySelector('.loading').classList.remove('d-block');
+            })
             .then(() => {
-                console.log('SUCCESS!');
+                thisForm.querySelector('.sent-message').classList.add('d-block');
+                thisForm.reset();
             }, (error) => {
-                console.log('FAILED...', error);
+                console.error(error);
+                displayError(thisForm, 'メール送信時にエラーが発生しました！お手数ですが再度ご送信ください。');
             });
     });
+
+    function displayError(thisForm, message) {
+        thisForm.querySelector('.loading').classList.remove('d-block');
+        const errorMessage = thisForm.querySelector('.error-message');
+        errorMessage.innerHTML = message;
+        errorMessage.classList.add('d-block');
+    }
 }
