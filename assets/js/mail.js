7beb114e9664773835791eb78e30447416d6d086
shunsei <kimshun1227@icloud.com>
2024-09-24 15:46:21 +0900

    Enable reCAPTCHA

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,17 +1,27 @@
 window.onload = function() {
     document.getElementById('contact-form').addEventListener('submit', function(event) {
+        // 送信を一時停止する
         event.preventDefault();
+
+        // reCAPTCHAにチェックが入っているか確認する
+        const recaptchaRespose = grecaptcha.getResponse();
+        if(recaptchaRespose.length === 0) {
+            displayError(this, 'reCAPTCHAにチェックを入れてください');
+            return;
+        }
 
         let thisForm = this;
 
+        // ローディング中のアニメーションを表示する
         thisForm.querySelector('.loading').classList.add('d-block');
         thisForm.querySelector('.error-message').classList.remove('d-block');
         thisForm.querySelector('.sent-message').classList.remove('d-block');
 
-        // generate a five digit number for the contact_number variable
+        // contact_numberに5桁の数字を入れる
+        // ドキュメントに目的は書かれていない
         thisForm.contact_number.value = Math.random() * 100000 | 0;
 
-        // these IDs from the previous steps
+        // メールを送信する
         emailjs.sendForm('service_mkob74b', 'template_mj7yns8', this)
             .finally(() => {
                 thisForm.querySelector('.loading').classList.remove('d-block');
@@ -21,7 +31,7 @@ window.onload = function() {
                 thisForm.reset();
             }, (error) => {
                 console.error(error);
-                displayError(thisForm, 'メール送信時にエラーが発生しました！お手数ですが再度ご送信ください。');
+                displayError(thisForm, 'メール送信時にエラーが発生しました。お手数ですが再度ご送信ください。');
             });
     });
 
